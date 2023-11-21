69ef85b9cab3a93173621784b9f13097c24b5a22
Lynn Chen <emmalynnchenn@gmail.com>
2023-11-21 16:27:53 -0600

    fixed indentation

--- inputval.asm
+++ inputval.asm
@@ -1,45 +1,45 @@
-    .data
-    new_line: .asciiz "\n"
-    input_prompt: .asciiz "your turn: "
-    inva_prompt: .asciiz "invalid input, please enter a number between 1 and 9"
-    try_again: .asciiz "press 0 to try again, anykey to exit: "
+    	.data
+    	new_line: .asciiz "\n"
+    	input_prompt: .asciiz "your turn: "
+    	inva_prompt: .asciiz "invalid input, please enter a number between 1 and 9"
+    	try_again: .asciiz "press 0 to try again, anykey to exit: "
 
-    .text
-    user_input:
-    li $v0, 4
+    	.text
+    	user_input:
+    	li $v0, 4
 	la $a0, input_prompt
 	syscall
 	li $v0, 5
 	syscall
-    j validation
+    	j validation
 
-    validation:
-    li $t0, 1
-    while:
-        beq $t0, 10, invalid
-        beq $v0, $t0, valid
-        addi $t0, $t0, 1
-        j while
-    j invalid
+    	validation:
+    	li $t0, 1
+   	while:
+        	beq $t0, 10, invalid
+        	beq $v0, $t0, valid
+        	addi $t0, $t0, 1
+        	j while
+    	j invalid
 
-    invalid:
-    li $v0, 4
+    	invalid:
+    	li $v0, 4
 	la $a0, inva_prompt
 	syscall
-    li $v0, 4
+    	li $v0, 4
 	la $a0, new_line
 	syscall
 	li $v0, 4
 	la $a0, try_again
 	syscall
-    li $v0, 5
+    	li $v0, 5
 	syscall
-    beq $v0, 0, user_input
-    j exit
+    	beq $v0, 0, user_input
+    	j exit
 
-    valid:
-    move $t1, $v0
+    	valid:
+    	move $t1, $v0
 
-    exit:
-    li $v0, 10
+    	exit:
+    	li $v0, 10
 	syscall
